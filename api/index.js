2ad210670f2067c94917a3827d407806f554093b
Supakrit65 <supakritaphonmaeklong@gmail.com>
2023-03-26 22:53:34 +0700

    add ui format for chat page fetch currently online users

--- api/index.js
+++ api/index.js
@@ -1,74 +1,74 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const UserModel = require('./models/User')
-const jsonWebToken = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const { request, response } = require('express')
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const UserModel = require("./models/User");
+const jsonWebToken = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { request, response } = require("express");
+const bcrypt = require("bcryptjs");
+const ws = require("ws");
 
 // Load environment variables
-dotenv.config()
+dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL
-const jwtSecretKey = process.env.JWT_SECRET_KEY
+const mongoUrl = process.env.MONGO_URL;
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 // Connect to MongoDB
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    console.log('Connected to MongoDB')
+    console.log("Connected to MongoDB");
   })
-  .catch(error => {
-    throw new Error(`Error connecting to MongoDB: ${error.message}`)
-  })
+  .catch((error) => {
+    throw new Error(`Error connecting to MongoDB: ${error.message}`);
+  });
 
-const app = express()
+const app = express();
 
 // Use the built-in middleware for parsing request bodies
-app.use(express.json())
+app.use(express.json());
 
 // Use cookie-parser middleware to parse cookies
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Use CORS middleware to allow cross-origin requests
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
+    credentials: true,
   })
-)
+);
 
-app.get('/test', (req, res) => {
-  res.json('test okay')
-})
+app.get("/test", (req, res) => {
+  res.json("test okay");
+});
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   /* Returns the user data associated with the JWT token in the 'token' cookie. */
-  const token = req.cookies.token
+  const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized No Token' })
+    return res.status(401).json({ message: "Unauthorized No Token" });
   }
 
   try {
-    const userData = jsonWebToken.verify(token, jwtSecretKey)
-    res.json(userData)
+    const userData = jsonWebToken.verify(token, jwtSecretKey);
+    res.json(userData);
   } catch (err) {
-    console.log(err)
-    res.status(401).json({ message: 'Unauthorized Invalid Token' })
+    console.log(err);
+    res.status(401).json({ message: "Unauthorized Invalid Token" });
   }
-})
+});
 
-
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   /* Authenticates the user with the provided username and password, and returns a JWT token as a cookie. */
-  const { username, password } = req.body
-  const user = await UserModel.findOne({ username: username })
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
   if (user) {
     // Compare the plaintext password with the stored hash
-    const isSame = await bcrypt.compare(password, user.password)
+    const isSame = await bcrypt.compare(password, user.password);
     if (isSame) {
       jsonWebToken.sign(
         { userId: user._id, username },
@@ -76,53 +76,124 @@ app.post('/login', async (req, res) => {
         {},
         (err, token) => {
           res
-            .cookie('token', token, {
+            .cookie("token", token, {
               httpOnly: true,
-              sameSite: 'none',
-              secure: true
+              sameSite: "none",
+              secure: true,
             })
             .json({
-              id: user._id
-            })
+              id: user._id,
+            });
         }
-      )
+      );
     }
   }
-})
+});
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   /* Creates a new user with the provided username and password, and returns a JWT token as a cookie. */
-  const { username, password } = req.body
+  const { username, password } = req.body;
 
   try {
     // Create a new user object and save it to the database
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
     // store hash in the database
-    const createdUser = new UserModel({ username, password: hashedPassword })
-    await createdUser.save()
+    const createdUser = new UserModel({ username, password: hashedPassword });
+    await createdUser.save();
     // Generate a JSON web token and set it as a cookie
     const token = jsonWebToken.sign(
       { userId: createdUser._id, username },
       jwtSecretKey
-    )
+    );
     res
-      .cookie('token', token, {
+      .cookie("token", token, {
         httpOnly: true,
-        sameSite: 'none',
-        secure: true
+        sameSite: "none",
+        secure: true,
       })
       .status(201)
       .json({
         id: createdUser._id,
-        message: `User ${createdUser._id} created successfully`
-      })
+        message: `User ${createdUser._id} created successfully`,
+      });
   } catch (error) {
-    console.log('Error creating user', error)
-    throw new Error('Internal server error')
+    console.log("Error creating user", error);
+    throw new Error("Internal server error");
   }
-})
+});
 
 const server = app.listen(4040, () => {
-  /* Starts the server and listens for incoming connections on port 4040. */
-  console.log('Server listening on port 4040')
-})
+  console.log("Server listening on port 4040");
+});
+
+const wss = new ws.WebSocketServer({ server: server });
+
+// Create a Set to store connected clients
+const clients = new Set();
+
+wss.on("connection", (connection, req) => {
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")[1];
+      if (token) {
+        try {
+          const decodedToken = jsonWebToken.verify(token, jwtSecretKey);
+          const { userId, username } = decodedToken;
+          console.log(`User connected: userId=${userId}, username=${username}`);
+          // Remove any existing client object with the same user ID
+          for (let c of clients) {
+            if (c.id === userId) {
+              clients.delete(c);
+              break;
+            }
+          }          
+          // Add the client to the list of online users
+          clients.add({
+            id: userId,
+            username: username,
+            ws: connection,
+          });
+          // Send the list of connected clients to all clients
+          [...clients].forEach((client) =>
+            client.ws.send(
+              JSON.stringify(
+                [...clients].map((c) => ({
+                  userId: c.id,
+                  username: c.username,
+                }))
+              )
+            )
+          );
+          // Add a 'close' event listener to the WebSocket connection
+          connection.on('close', () => {
+            console.log(`User disconnected: userId=${userId}, username=${username}`);
+            // Remove the client from the list of online users
+            for (let c of clients) {
+              if (c.id === userId) {
+                clients.delete(c);
+                break;
+              }
+            } 
+            // Send the updated list of connected clients to all clients
+            [...clients].forEach((client) =>
+              client.ws.send(
+                JSON.stringify(
+                  [...clients].map((c) => ({
+                    userId: c.id,
+                    username: c.username,
+                  }))
+                )
+              )
+            );
+          });
+        } catch (err) {
+          console.log("Error verifying JWT:", err);
+        }
+      }
+    }
+  }
+});
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,66 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "./UserContext";
 
-function Chat () {
+function Chat() {
+  // State variables
+  const [ws, setWs] = useState(null); // WebSocket connection
+  const [onlinePeople, setOnlinePeople] = useState({}); // Online users
+  const { username, id } = useContext(UserContext); // Current user data
+
+  // Connect to WebSocket server on component mount
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:4040");
+    setWs(ws);
+    ws.addEventListener("message", handleMessage);
+  }, []);
+
+  // Function to transform array of users into object of users
+  function showOnlinePeople(peopleArray) {
+    const people = {};
+    peopleArray.forEach(({ userId, username }) => {
+      // Don't add current user to online people list
+      if (userId !== id) {
+        people[userId] = username;
+      }
+    });
+    console.log('showOnlinePeopl', people);
+    setOnlinePeople(people);
+  }
+
+  // Handle WebSocket message event
+  function handleMessage(event) {
+    const messageData = JSON.parse(event.data);
+    console.log(messageData);
+    showOnlinePeople(messageData);
+  }
+
   return (
-    <div className='h-screen flex flex-col'>
-      <div className='flex-1 flex'>
-        <div className='w-1/3 h-full bg-green-600 p-4'>
-          <div className='flex justify-between items-center mb-4'>
-            <h2 className='text-white font-bold text-lg'>Chat List</h2>
-            <span className='text-white'>Active Person</span>
+    <div className="h-screen flex flex-col">
+      <div className="flex-1 flex">
+        {/* Chat list */}
+        <div className="w-1/3 h-full bg-stone-600 p-4">
+          {/* Header */}
+          <div className="flex justify-between items-center mb-4">
+            {/* Chat list title and icon */}
+            <div className="flex items-center">
+              <h2 className="text-white font-bold text-lg">Chat List</h2>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/811/811476.png"
+                alt="chat icon"
+                className="w-6 h-6 ml-2"
+              />
+            </div>
+            {/* Active person label */}
+            <span className="text-white">Active Person</span>
           </div>
-          <div className='bg-white p-2 rounded-lg'>
-            <ul>
-              <li className='flex items-center space-x-2'> 
-                <img
-                  src='https://picsum.photos/id/237/50/50'
-                  alt='avatar'
-                  className='rounded-full'
-                />
-                <div>
-                  <h3 className='font-bold'>John Doe</h3>
-                  <p className='text-gray-600 text-sm'>Hello there</p>
-                </div>
-              </li>
-              <li className='flex items-center space-x-2'>
-                <img
-                  src='https://picsum.photos/id/238/50/50'
-                  alt='avatar'
-                  className='rounded-full'
-                />
-                <div>
-                  <h3 className='font-bold'>Jane Doe</h3>
-                  <p className='text-gray-600 text-sm'>Hi, how are you?</p>
-                </div>
-              </li>
+          {/* Online people list */}
+          <div className="bg-white p-2 rounded-lg">
+            <ul className="mt-3 mx-3">
+              {Object.values(onlinePeople).map((u) => (
+                <li
+                  key={u}
+                  className={
+                    "flex items-center space-x-3 mb-3 p-2 rounded-lg bg-green-200"
+                  }
+                >
+                  <img
+                    src="https://picsum.photos/id/237/50/50"
+                    alt="avatar"
+                    className="rounded-full"
+                  />
+                  <div>
+                    <h3 className="font-bold">{u}</h3>
+                    <p className="text-gray-600 text-sm">Hello there</p>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
 
-        <div className='w-2/3 h-full bg-gray-100 p-4'>
-          <div className='flex flex-col h-full'>
-            <div className='flex-grow overflow-y-scroll'>
+        {/* Chat area */}
+        <div className="w-2/3 h-full bg-gray-200 p-4">
+          <div className="flex flex-col h-full">
+            {/* Messages */}
+            <div className="flex-grow overflow-y-scroll">
               Message with selected person
             </div>
-            <div className='flex gap-2'>
+            {/* Message input */}
+            <div className="flex gap-2">
               <input
-                type='text'
-                className='bg-white border p-2 flex-grow'
-                placeholder='Type your message here'
+                type="text"
+                className="bg-white border p-2 flex-grow"
+                placeholder="Type your message here"
               />
-              <button className='bg-sky-500/75 rounded-lg p-2 text-cyan-100'>
+              <button className="bg-sky-500/75 rounded-lg p-2 text-cyan-100">
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth='1.5'
-                  stroke='currentColor'
-                  className='w-6 h-6'
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
                   />
                 </svg>
               </button>
@@ -69,7 +113,7 @@ function Chat () {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
